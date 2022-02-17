15aa534e52c2fbf3ff5676e759f3399488fcf3b5
Priyanka <priyankadombale27@gmail.com>
2022-02-18 00:16:02 +0530

    Added:Welcome Message

---
+++ src/com/blz/LineComparison.java
@@ -0,0 +1,10 @@
+package com.blz;
+
+public class LineComparison {
+
+	public static void main(String[] args) {
+		System.out.println(" Welcome in Line Comparison..!!");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module Day8_Line_Comparison_OOPS {
+}
